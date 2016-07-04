abfe883c42db926c96a8139cea81baa379077b8f
taihejin <taihejin2008@126.com>
2016-07-04 08:31:03 +0800

    text justification

--- leetcode/java/src/main/java/P68.java
+++ leetcode/java/src/main/java/P68.java
@@ -10,69 +10,46 @@ import java.util.List;
 
 public class P68 {
 
-    // the code is bull shit, try to refactoring!!!
     public List<String> fullJustify(String[] words, int maxWidth) {
         List<String> list = new ArrayList<String>();
-        StringBuilder builder = new StringBuilder();
         int low = 0;
-        int high = 0;
-        int size = 0;
-        int gap = 0;
-        for (; high < words.length; high++) {
-            size += gap + words[high].length();
-            gap = 1;
-            if (size == maxWidth) {
-                for (int k = low; k < high; k++) {
-                    builder.append(words[k]).append(' ');
+        int size = words.length;
+        while (low < size) {
+            int high = low + 1;
+            int curr = words[low].length();
+            for (; high < size && curr + 1 + words[high].length() <= maxWidth; high++) {
+                curr += 1 + words[high].length();
+            }
+            String line = words[low];
+            if (high == size) { // last line
+                for (int k = low + 1; k < high; k++) {
+                    line += " " + words[k];
                 }
-                builder.append(words[high]);
-                list.add(builder.toString());
-                builder.delete(0, builder.length());
-                gap = 0;
-                size = 0;
-                low = high + 1;
-            } else if (size > maxWidth) {
-                size -= (gap + words[high].length());
-                high--;
-                if (low == high) {
-                    for (int k = 0; k < maxWidth - size; k++) {
-                        builder.append(' ');
-                    }
-                    builder.append(words[low]);
-                    list.add(builder.toString());
-                    builder.delete(0, builder.length());
-                } else {
-                    int evenly = (maxWidth - size) / (high - low) + 1;
-                    int rest = (maxWidth - size) % (high - low);
-                    for (int k = low; k < high; k++) {
-                        builder.append(words[k]);
-                        for (int l = 0; l < evenly; l++) {
-                            builder.append(' ');
-                        }
-                        if (rest-- > 0) {
-                            builder.append(' ');
-                        }
-                    }
-                    builder.append(words[high]);
-                    list.add(builder.toString());
-                    builder.delete(0, builder.length());
+                while (line.length() < maxWidth) {
+                    line += " ";
                 }
-                gap = 0;
-                size = 0;
-                low = high + 1;
+            } else {
+                int whiteNum = maxWidth - curr;
+                int whiteSlots = high - low - 1;
+                if (whiteSlots == 0) { // only one word
+                    while (line.length() < maxWidth) {
+                        line += " ";
+                    }
+                } else { // normal condition
+                    int extraWhite = whiteNum % whiteSlots;
+                    for (int i = low + 1; i < high; i++) {
+                        for (int k = 0; k <= whiteNum / whiteSlots; k++) {
+                            line += " ";
+                        }
+                        if (extraWhite-- > 0) {
+                            line += " ";
+                        }
+                        line += words[i];
+                    }
+                }
             }
-        }
-        if (low < words.length) {
-            for (int k = low; k < words.length; k++) {
-                builder.append(words[low]).append(' ');
-            }
-            if (builder.length() > maxWidth) {
-                builder.delete(maxWidth + 1, builder.length());
-            }
-            for (int k = 0; k < maxWidth - size; k++) {
-                builder.append(' ');
-            }
-            list.add(builder.toString());
+            low = high;
+            list.add(line);
         }
         return list;
     }
@@ -99,7 +76,7 @@ public class P68 {
 
     @Test
     public void test4() {
-        String[] text = {"12", "12", "123", "12"};
+        String[] text = {"12", "12", "23", "12"};
         List<String> ret = fullJustify(text, 8);
         Assert.assertEquals(2, ret.size());
     }
