ddaf77b9fb305749b4802a242dc226570c85a88c
taihejin <taihejin2008@126.com>
2016-07-19 11:13:26 +0800

    format bubble sort

--- algorithm/src/main/java/base/BubbleSort.java
+++ algorithm/src/main/java/base/BubbleSort.java
@@ -15,7 +15,7 @@ public class BubbleSort {
     }
 
     // very smart code, yes they do!
-    public void bubbleSort(int[] nums) {
+    public void bubbleSort1(int[] nums) {
         if (nums == null || nums.length <= 1) {
             return;
         }
@@ -27,6 +27,19 @@ public class BubbleSort {
                 if (nums[k] > nums[k + 1]) {
                     swap(nums, k, k + 1);
                     tail = k;
+                }
+            }
+        }
+    }
+
+    public void bubbleSort(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j, j + 1);
                 }
             }
         }
