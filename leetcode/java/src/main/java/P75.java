091a0f82df9fee17fa9d6883ccd33ca2520dafd3
taihejin <taihejin2008@126.com>
2016-07-19 17:59:01 +0800

    sort color by changing

--- leetcode/java/src/main/java/P75.java
+++ leetcode/java/src/main/java/P75.java
@@ -7,27 +7,23 @@ import org.junit.Test;
  */
 public class P75 {
 
+    public void swap(int[] nums, int low, int high) {
+        int tmp = nums[low];
+        nums[low] = nums[high];
+        nums[high] = tmp;
+    }
+
     public void sortColors(int[] nums) {
         if (nums == null || nums.length <= 1) return;
-        int redCount = 0;
-        int whiteCount = 0;
-        int blueCount = 0;
+        int low = 0;
+        int high = nums.length - 1;
         for (int i = 0; i < nums.length; i++) {
-            switch (nums[i]) {
-                case 0: redCount++; break;
-                case 1: whiteCount++; break;
-                case 2: blueCount++; break;
+            while (nums[i] == 2 && i < high){
+                swap(nums, i, high--);
             }
-        }
-        int k = 0;
-        for (int i = 0; i < redCount; i++) {
-            nums[k++] = 0;
-        }
-        for (int i = 0; i < whiteCount; i++) {
-            nums[k++] = 1;
-        }
-        for (int i = 0; i < blueCount; i++) {
-            nums[k++] = 2;
+            while (nums[i] == 0 && i > low) {
+                swap(nums, i, low++);
+            }
         }
     }
 
@@ -50,6 +46,13 @@ public class P75 {
         int[] nums = {2, 1, 1, 0};
         sortColors(nums);
         Assert.assertArrayEquals(new int[]{0, 1, 1, 2}, nums);
+    }
+
+    @Test
+    public void test3() {
+        int[] nums = {2, 2, 0, 0};
+        sortColors(nums);
+        Assert.assertArrayEquals(new int[]{0, 0, 2, 2}, nums);
     }
 }
 
