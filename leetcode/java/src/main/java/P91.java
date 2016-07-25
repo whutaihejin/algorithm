2f3ab7e5783c3273afbc104259710f1b24e48440
zhanglong <zhanglong@staff.weibo.com>
2016-07-25 11:31:11 +0800

    fix decoding ways

--- leetcode/java/src/main/java/P91.java
+++ leetcode/java/src/main/java/P91.java
@@ -18,11 +18,12 @@ public class P91 {
     }
 
     public int numDecodings(String s) {
-        int low = 1;
+        if (s == null || s.isEmpty()) return 0;
+        int low = s.charAt(s.length() - 1) == '0' ? 0 : 1;
         int high = 1;
         for (int k = s.length() - 2; k >= 0; k--) {
-            boolean valid = ((s.charAt(k) - '0') * 10 + s.charAt(k + 1) - '0') <= 26;
-            int val = low + (valid ? high : 0);
+            char curr = s.charAt(k);
+            int val = (curr == '0' ? 0 : low + (((curr - '0') * 10 + s.charAt(k + 1) - '0') <= 26 ? high : 0));
             high = low;
             low = val;
         }
@@ -38,5 +39,24 @@ public class P91 {
         Assert.assertEquals(1, numDecodings("66"));
         Assert.assertEquals(3, numDecodings("121"));
         Assert.assertEquals(5, numDecodings("1213"));
+    }
+
+    @Test
+    public void test1() {
+        Assert.assertEquals(0, numDecodings(""));
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(0, numDecodings("0"));
+        Assert.assertEquals(1, numDecodings("1"));
+        Assert.assertEquals(2, numDecodings("11"));
+        Assert.assertEquals(1, numDecodings("110"));
+        Assert.assertEquals(0, numDecodings("00"));
+        Assert.assertEquals(0, numDecodings("100"));
+        Assert.assertEquals(0, numDecodings("1100"));
+        Assert.assertEquals(0, numDecodings("1100111"));
+        Assert.assertEquals(2, numDecodings("11011"));
+        Assert.assertEquals(1, numDecodings("27"));
     }
 }
