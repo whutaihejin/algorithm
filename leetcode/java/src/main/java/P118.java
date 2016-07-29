cc15c9f5f823dc6253bb09d0faa3fb849ad60800
taihejin <taihejin2008@126.com>
2016-07-29 17:46:34 +0800

    pascal's triangle

---
+++ leetcode/java/src/main/java/P118.java
@@ -0,0 +1,58 @@
+/**
+ * Created by taihejin on 16-7-28.
+ */
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class P118 {
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        if (numRows <= 0) return ret;
+        ret.add(Arrays.asList(1));
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> level = ret.get(i - 1);
+            List<Integer> item = new ArrayList<Integer>();
+            item.add(1);
+            for (int k = 1; k < i; k++) {
+                item.add(level.get(k) + level.get(k - 1));
+            }
+            item.add(1);
+            ret.add(item);
+        }
+        return ret;
+    }
+
+    @Test
+    public void test0() {
+        List<List<Integer>> ret = generate(0);
+        Assert.assertEquals(true, ret.isEmpty());
+    }
+
+    @Test
+    public void test1() {
+        List<List<Integer>> ret = generate(1);
+        Assert.assertEquals(1, ret.size());
+    }
+
+    @Test
+    public void test2() {
+        List<List<Integer>> ret = generate(2);
+        Assert.assertEquals(2, ret.size());
+    }
+
+    @Test
+    public void test3() {
+        List<List<Integer>> ret = generate(5);
+        Assert.assertEquals(5, ret.size());
+    }
+
+    @Test
+    public void test4() {
+        List<List<Integer>> ret = generate(10);
+        Assert.assertEquals(10, ret.size());
+    }
+}
---
+++ leetcode/java/src/main/java/P119.java
@@ -0,0 +1,66 @@
+/**
+ * Created by taihejin on 16-7-28.
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class P119 {
+
+    public List<Integer> getRow(int rowIndex) {
+        if (rowIndex < 0) return Arrays.asList();
+        Integer[] arr = new Integer[rowIndex + 1];
+        Arrays.fill(arr, 0, rowIndex + 1, 0);
+        Arrays.fill(arr, 0, 1, 1);
+        for (int row = 1; row <= rowIndex; row++) {
+            for (int high = row; high >= 1; high--) {
+                arr[high] += arr[high - 1];
+            }
+        }
+        return Arrays.asList(arr);
+    }
+
+    @Test
+    public void test0() {
+        Assert.assertEquals(true, getRow(-1).isEmpty());
+        List<Integer> ret = getRow(0);
+        Assert.assertEquals(1, ret.size());
+        Assert.assertEquals(1, ret.get(0).intValue());
+    }
+
+    @Test
+    public void test1() {
+        List<Integer> ret = getRow(1);
+        Assert.assertEquals(2, ret.size());
+        Assert.assertEquals(1, ret.get(0).intValue());
+        Assert.assertEquals(1, ret.get(1).intValue());
+    }
+
+    @Test
+    public void test2() {
+        List<Integer> ret = getRow(2);
+        Assert.assertEquals(3, ret.size());
+        Assert.assertEquals(1, ret.get(0).intValue());
+        Assert.assertEquals(2, ret.get(1).intValue());
+        Assert.assertEquals(1, ret.get(2).intValue());
+    }
+
+    @Test
+    public void test3() {
+        List<Integer> ret = getRow(3);
+        Assert.assertEquals(4, ret.size());
+        Assert.assertEquals(1, ret.get(0).intValue());
+        Assert.assertEquals(3, ret.get(1).intValue());
+        Assert.assertEquals(3, ret.get(2).intValue());
+        Assert.assertEquals(1, ret.get(3).intValue());
+    }
+
+    @Test
+    public void test4() {
+        List<Integer> ret = getRow(6);
+        Assert.assertEquals(7, ret.size());
+    }
+}
