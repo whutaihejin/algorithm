9afb90f00d3281d5c42aad47e19850a91d9c3ea0
taihejin <taihejin2008@126.com>
2016-08-07 19:14:18 +0800

    ineffective

--- leetcode/java/src/main/java/P139.java
+++ leetcode/java/src/main/java/P139.java
@@ -11,13 +11,44 @@ import java.util.Set;
 public class P139 {
 
     // Time Limit Exceeded
-    public boolean wordBreak(String s, Set<String> wordDict) {
+    public boolean wordBreakRecursive(String s, Set<String> wordDict) {
         for (int k = 1; k <= s.length(); k++) {
             if (wordDict.contains(s.substring(0, k)) && wordBreak(s.substring(k), wordDict)) {
                 return true;
             }
         }
         return s.isEmpty() ? true : false;
+    }
+
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        if (s.isEmpty()) return wordDict.contains(s);
+        int size = s.length();
+        boolean[][] matrix = new boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            matrix[i][i] = wordDict.contains(s.substring(i, i + 1)) ? true : false;
+        }
+        for (int len = 2; len <= size; len++) {
+            for (int i = 0; i <= size - len; i++) {
+                if (wordDict.contains(s.substring(i, i + len))) {
+                    matrix[i][i + len - 1] = true;
+                    continue;
+                }
+                for (int k = i; k < i + len - 1; k++) {
+                    if (matrix[i][k] && matrix[k + 1][i + len - 1]) {
+                        matrix[i][i + len - 1] = true;
+                    }
+                }
+            }
+        }
+        return matrix[0][size - 1];
+    }
+
+    @Test
+    public void testSelf() {
+        Set<String> wordDict = new HashSet<String>(Arrays.asList("ab", "c"));
+        Assert.assertEquals(false, wordDict.contains(null));
+        Assert.assertEquals(false, wordDict.contains(""));
+        Assert.assertEquals(true, wordBreak("abc", wordDict));
     }
 
     @Test
@@ -34,7 +65,7 @@ public class P139 {
     @Test
     public void test1() {
         Set<String> wordDict = new HashSet<String>(Arrays.asList("leet", "code"));
-        Assert.assertEquals(true, wordBreak("", wordDict));
+        Assert.assertEquals(false, wordBreak("", wordDict));
         Assert.assertEquals(false, wordBreak("whu", wordDict));
     }
 
