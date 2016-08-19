509a430edf64c0e513477e2053ba22a28cbf816b
taihejin <taihejin2008@126.com>
2016-08-19 14:12:34 +0800

    maximum product subarray

--- leetcode/java/src/main/java/P152.java
+++ leetcode/java/src/main/java/P152.java
@@ -1,25 +1,21 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.lang.annotation.Target;
 
 /**
  * Created by taihejin on 16-8-18.
  */
 public class P152 {
 
-    // Time Limit Exceeded
     public int maxProduct(int[] nums) {
-        int product = Integer.MIN_VALUE;
-        for (int len = 1; len <= nums.length; len++) {
-            for (int i = 0; i <= nums.length - len; i++) {
-                int j = i + len - 1;
-                int current = 1;
-                for (int k = i; k <= j; k++) {
-                    current *= nums[k];
-                }
-                product = Math.max(product, current);
-            }
+        int product = nums[0];
+        int max = product, min = product;
+        for (int i = 1; i < nums.length; i++) {
+            int left = max * nums[i];
+            int right = min * nums[i];
+            max = Math.max(left, Math.max(nums[i], right));
+            min = Math.min(left, Math.min(nums[i], right));
+            product = Math.max(product, max);
         }
         return product;
     }
@@ -36,11 +32,17 @@ public class P152 {
     @Test
     public void test1() {
         Assert.assertEquals(6, maxProduct(new int[]{2, 3, -2, 4}));
+        Assert.assertEquals(16, maxProduct(new int[]{2, 3, -2, 0, 4, 4}));
     }
 
     @Test
     public void test2() {
         Assert.assertEquals(2, maxProduct(new int[]{0, 2}));
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertEquals(12, maxProduct(new int[]{-4, -3, -2}));
     }
 
 }
